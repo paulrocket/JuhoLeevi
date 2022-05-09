ddb5db21ae1871bd5a2ad3fd498979ca0410c5c4
Leevi12 <leevi.sallantaus@gmail.com>
2022-05-09 15:26:22 +0300

    alku

--- MapApplication/wwwroot/js/site.js
+++ MapApplication/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+var map = new L.Map('leaflet', {
+    center: [0, 0],
+    zoom: 0,
+    layers: [
+        new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            'attribution': 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+        })
+    ]
+});
