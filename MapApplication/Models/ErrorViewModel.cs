28baf69968f6f03ea600317c202e1a4a371095a0
Leevi12 <leevi.sallantaus@gmail.com>
2022-05-09 15:06:01 +0300

    v5

---
+++ MapApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MapApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
