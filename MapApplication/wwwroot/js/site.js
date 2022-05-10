8511a68ca4ae0fc4860c878e4784d9c6c431e503
Leevi12 <leevi.sallantaus@gmail.com>
2022-05-10 15:19:28 +0300

    leaflet

--- MapApplication/wwwroot/js/site.js
+++ MapApplication/wwwroot/js/site.js
@@ -4,7 +4,7 @@ var map = L.map('map').setView([0, 0], 1);
 var tilelayer = new L.TileLayer('https://api.maptiler.com/maps/basic/{z}/{x}/{y}.png?key=EVuUOQm9Knxh84jI671s', {
         tileSize: 512,
         zoomOffset: -1,
-        minZoom: 1,
+        minZoom: 2,
         attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
         crossOrigin: true
     }).addTo(map);
