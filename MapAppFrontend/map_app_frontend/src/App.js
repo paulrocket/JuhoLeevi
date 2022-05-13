713b17cfef5cff32425fdd3ca75f5a556d95b7d0
Leevi12 <leevi.sallantaus@gmail.com>
2022-05-13 14:30:49 +0300

    leaflet map not working properly

--- MapAppFrontend/map_app_frontend/src/App.js
+++ MapAppFrontend/map_app_frontend/src/App.js
@@ -11,12 +11,14 @@ export default function App() {
       <MapContainer center={[50.5, 30.5]} zoom={2}scrollWheelZoom={true}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           zoomOffset={1}
           tileSize={512}
           minZoom={2}
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
+
       </MapContainer>
+
 
   );
 }
