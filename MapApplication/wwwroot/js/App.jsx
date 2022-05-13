d6295262fa1c8a7f4e7df4816d066b4b5b5c5686
Leevi12 <leevi.sallantaus@gmail.com>
2022-05-13 10:16:33 +0300

    react

--- MapApplication/wwwroot/js/App.jsx
+++ MapApplication/wwwroot/js/App.jsx
@@ -1,7 +1,9 @@
-﻿function App() {
+﻿import Test from "./Test.js";
+
+function App() {
     return (
         <div className="App">
-            <h1>test</h1>
+            <Test/>
         </div>
     );
 }
---
+++ MapApplication/wwwroot/js/Test.jsx
@@ -0,0 +1,11 @@
+﻿const Test = (props) => {
+    let name = "Test";
+    if (props.name) {
+        name = props.name;
+    }
+    return (
+        <h1>Hello {name}</h1>
+    )
+}
+
+export default Test;
