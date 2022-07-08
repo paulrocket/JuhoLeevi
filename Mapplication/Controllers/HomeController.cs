9acfbfb03d163e09fad683c83e4ea3f2ff720092
Leevi12 <leevi.sallantaus@gmail.com>
2022-07-08 13:23:42 +0300

    Moving stuff from previous work

--- Mapplication/Controllers/HomeController.cs
+++ Mapplication/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace Mapplication.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Login()
         {
             return View();
         }
--- Mapplication/wwwroot/js/site.js
+++ Mapplication/wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const map = L.map('map').setView([51.505, -0.09], 2);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 4,
+    minZoom: 2,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
+
